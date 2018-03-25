8545a742f5ccf54371df711ede1e76be5a2cc86b
Robert Raboud <robert.raboud@randreng.com>
2018-03-25 16:00:10 -0400

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TestCarouselComponent } from './test-carousel/test-carousel.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCarouselComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
